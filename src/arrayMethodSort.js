dea8e097730e729f2b64619babdf5938f68538c8
vitalii <vitalii.myhailyk.w@gmail.com>
2025-02-11 00:52:27 +0200

    task solved

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,21 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    for (let j = 0; j < this.length - 1; j++) {
+      for (let i = j + 1; i < this.length; i++) {
+        const compare =
+          arguments.length === 0
+            ? (String(this[j]) > String(this[i])) - (String(this[j]) < String(this[i]))
+            : compareFunction(this[j], this[i]);
+
+        if (compare > 0) {
+          [this[i], this[j]] = [this[j], this[i]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
