866c96ef493aec25900ac4b88f8347700f1f0259
vitalii <vitalii.myhailyk.w@gmail.com>
2025-02-11 05:45:13 +0200

    add quick sort

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -9,7 +9,8 @@ function applyCustomSort() {
       for (let i = j + 1; i < this.length; i++) {
         const compare =
           arguments.length === 0
-            ? (String(this[j]) > String(this[i])) - (String(this[j]) < String(this[i]))
+            ? (String(this[j]) > String(this[i])) -
+            (String(this[j]) < String(this[i]))
             : compareFunction(this[j], this[i]);
 
         if (compare > 0) {
@@ -19,6 +20,33 @@ function applyCustomSort() {
     }
 
     return this;
+    /*  if (this.length <= 1) {
+      return this;
+    }
+
+    const pivot = this[this.length - 1];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < this.length - 1; i++) {
+      const compare = compareFunction
+        ? compareFunction(this[i], pivot)
+        : +(String(this[i]) > String(pivot)); // Порівнюємо рядково 
+
+     if (compare > 0) {
+        right[right.length] = this[i];
+
+      } else {
+        left[left.length] = this[i];
+      }
+    }
+
+    this.length = 0;
+    this.push(...left.sort2(compareFunction),
+     pivot,
+     ...right.sort2(compareFunction));
+     
+    return this; */
   };
 }
 
